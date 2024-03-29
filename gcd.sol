4fb6a6aa255cb8842fe03215aea8c4816206d9bc
saivivek08 <110596795+saivivek08@users.noreply.github.com>
2024-03-29 11:46:02 +0530

    Update gcd.sol

--- gcd.sol
+++ gcd.sol
@@ -1,1 +1,14 @@
+pragma solidity ^0.8.14;
 
+contract GCDFinder {
+    function findGCD(uint256 a, uint256 b) external pure returns (uint256) {
+        require(a > 0 && b > 0, "Numbers must be greater than zero");
+        while (b != 0) {
+            uint256 temp = b;
+            b = a % b;
+            a = temp;
+        }
+
+        return a;
+    }
+}
